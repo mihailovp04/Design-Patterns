d0322803966257ac0c2f0401fd543a72629904e1
mihailovp04 <mihailovp04@gmail.com>
2025-09-24 11:55:19 +0300

    basic

---
+++ lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Hello, Jerry");
+
+void PrintABC()
+{
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+}
+
+PrintABC();
+Thread.Sleep(500); 
+PrintABC();
+Thread.Sleep(500);
+PrintABC();
+
+void A()
+{
+    Console.WriteLine("Функция A");
+    B();
+    C();
+}
+
+void B()
+{
+    Console.WriteLine("Функция B");
+}
+
+void C()
+{
+    Console.WriteLine("Функция C");
+}
+
+A(); 
+A(); 
